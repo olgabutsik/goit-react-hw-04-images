c1533abbe574bf75ed84b70be0e168fa95bb32d0
Olha <cukbu30@gmai.com>
2023-02-22 15:52:33 +0200

    add hooks to Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,34 +1,29 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import css from './Modal.module.css';
 
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onCloseByEsc);
-  }
+export function Modal({ largeImageURL, onCloseModal }) {
+  useEffect(() => {
+    const onCloseByEsc = e => {
+      if (e.code === 'Escape') {
+        onCloseModal();
+      }
+    };
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onCloseByEsc);
-  }
+    window.addEventListener('keydown', onCloseByEsc);
+    return () => window.removeEventListener('keydown', onCloseByEsc);
+  }, [onCloseModal]);
 
-  onCloseByEsc = e => {
-    if (e.code === 'Escape') {
-      this.props.onCloseModal();
-    }
-  };
-
-  handleClick = e => {
+  const handleClick = e => {
     if (e.currentTarget === e.target) {
-      this.props.onCloseModal();
+      onCloseModal();
     }
   };
 
-  render() {
-    return (
-      <div className={css.Overlay} onClick={this.handleClick}>
-        <div className={css.Modal}>
-          <img src={this.props.largeImageURL} alt="Large" />
-        </div>
+  return (
+    <div className={css.Overlay} onClick={handleClick}>
+      <div className={css.Modal}>
+        <img src={largeImageURL} alt="Large" />
       </div>
-    );
-  }
+    </div>
+  );
 }
